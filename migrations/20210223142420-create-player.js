dfe5c262681f417b77aea639e9e54f8fc59ce0ef
Khatium <maxime.blay98@gmail.com>
2021-02-23 15:28:39 +0100

    push minor changes (hope it doesnt break anything

---
+++ migrations/20210223142420-create-player.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Players', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      gameWin: {
+        type: Sequelize.NUMBER
+      },
+      gameLost: {
+        type: Sequelize.NUMBER
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Players');
+  }
+};
