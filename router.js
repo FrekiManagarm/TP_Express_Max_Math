86e2c8b13775d168d06afe4751fb8f2491941f66
Freki Managarm <mathieu.chambaud@ynov.com>
2021-02-23 18:59:54 +0100

    ajout du front-end

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const gameRouter = require('./routers/game.js');
 const playerRouter = require('./routers/player.js');
 
+
 router.use('/games', gameRouter);
 router.use('/players', playerRouter);
 
