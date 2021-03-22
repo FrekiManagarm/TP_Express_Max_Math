7fc66cc8acfbae901049b60cb214b7f4df0799cc
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-03-22 09:34:26 +0100

    poursuit les views

--- front/src/pages/Games/301/Game301.jsx
+++ front/src/pages/Games/301/Game301.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import Navbar from '../../../components/Navbar/Navbar';
 
 const Game301 = () => {
     return (
         <div>
-            
+            <Navbar/>
         </div>
     )
 }
--- front/src/pages/Games/DartGame/DartGame.jsx
+++ front/src/pages/Games/DartGame/DartGame.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import axios from 'axios';
+import Navbar from '../../../components/Navbar/Navbar';
 
 const DartGame = () => {
     return (
         <div>
-            
+            <Navbar />
         </div>
     )
 }
