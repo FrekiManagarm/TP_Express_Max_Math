b49034b890a1ba9649ee2a3d0328ff37159eb522
FrekiManagarm <mathieu.chambaud@ynov.com>
2021-03-28 22:46:32 +0200

    j'ai fait mon boulot

--- front/src/components/Navbar/Navbar.style.js
+++ front/src/components/Navbar/Navbar.style.js
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 export const ListWrapper = styled.li`
-    
+    list-style-type: none;
+
+    li {
+        text-align: center;
+        margin: 15px auto;
+    }
 `;
 
 export const NavWrapper = styled.nav`
